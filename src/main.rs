fee18b998bb3a3995f058e23bbc27b6419329d15
jbgriesner <jean.benoit.griesner@gmail.com>
2021-12-22 23:04:57 +0100

    add error outpustream

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,13 @@ fn main() {
 
     let config = Config::new(&args).unwrap_or_else(
         |err| {
-            println!("Parsing error: {}", err);
+            eprintln!("Parsing error: {}", err);
             process::exit(1);
         }
     );
 
     if let Err(e) = topgrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }    
 }
