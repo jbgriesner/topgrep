c6d3eeef94431d9080ade945964936d053ea19bb
jbgriesner <jean.benoit.griesner@gmail.com>
2021-12-22 21:41:12 +0100

    add config struct + modularity

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use topgrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(
+        |err| {
+            println!("Parsing error: {}", err);
+            process::exit(1);
+        }
+    );
 
-    println!("searching for {}", query);
-    println!("In file {}", filename);
+    println!("searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename)
-                                .expect("Error reading file");
-
-    println!("With text:\n{}", contents);
-
+    if let Err(e) = topgrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }    
 }
+
+
